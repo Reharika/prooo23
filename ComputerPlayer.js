2fa3bc24e7319eeb841533d497a284e9a64074b7
Reharika <84969493+Reharika@users.noreply.github.com>
2021-06-27 20:03:17 +0530

    Add files via upload

---
+++ ComputerPlayer.js
@@ -0,0 +1,26 @@
+class ComputerPlayer {
+    constructor(x,y,width,height){
+        var options = {
+            isStatic = true
+        };
+        
+        this.Body =Bodies.rectangle(x,y,width,height,options);
+        this.width = width
+        this.height = height
+        this.image = loadImage("./assests/player.png")
+
+        World.add(world,this.Body);
+    }
+    display() {
+        var pos = this.Body.position;
+        var angle = this.Body.angle;
+    
+        push()
+        translate(pos.x,pos.y)
+        rotate(angle);
+        imageMode(CENTER)
+        image(this.image,0,0,this.width,this.height);
+        pop()
+    }
+    
+}
